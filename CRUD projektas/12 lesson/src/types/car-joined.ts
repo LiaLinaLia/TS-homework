e46df4c5158ced8cd97f4abe103432d8a0e27aed
LiaLinaLia <lina.suminaite@codeacademylt.onmicrosoft.com>
2022-08-26 21:28:00 +0300

    feat: 12 and 13 homework

---
+++ CRUD projektas/12 lesson/src/types/car-joined.ts
@@ -0,0 +1,10 @@
+import Car from './car';
+import Model from './model';
+import Brand from './brand';
+
+type CarJoined = Omit<Car, 'modelId'> & {
+    brand: Brand['title'],
+    model: Model['title'],
+};
+
+export default CarJoined;
